550f235363b303f958e4a7e3e41ba223b7f5fb94
rafi-arlab <rafi@thearlab.com>
2026-01-02 17:10:51 +0200

    Initial commit

---
+++ Assets/Scripts/Util/HintsManager.js
@@ -0,0 +1,27 @@
+//@input Component.Image[] hints
+
+global.ShowHint = (id, delay, callback) => {
+    if (id < 0 || id > script.hints.length - 1) {
+        print("WARNING: Hint with id " + id + " does not exist.")
+        return
+    }
+    let fadeInHint = new CustomTween({
+        delay: delay,
+        onUpdate: (progress) => { script.hints[id].mainPass.baseColor = new vec4(1, 1, 1, progress) },
+        onComplete: callback
+    })
+    fadeInHint.start()
+}
+
+global.HideHint = (id, delay, callback) => {
+    if (id < 0 || id > script.hints.length - 1) {
+        print("WARNING: Hint with id " + id + " does not exist.")
+        return
+    }
+    let fadeOutHint = new CustomTween({
+        delay: delay,
+        onUpdate: (progress) => { script.hints[id].mainPass.baseColor = new vec4(1, 1, 1, 1 - progress) },
+        onComplete: callback
+    })
+    fadeOutHint.start()
+}
